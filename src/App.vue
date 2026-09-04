16f8865a86bf02af2e9043708818e326a0a62de3
powemetal <francisbvrt@gmail.com>
2026-09-04 11:24:34 -0400

    [Feat] Ajout du debut l'interface des palettes, affichage des palettes dans la barre de palettes

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,17 @@ import Navigation from "./components/Navigation.vue";
 </script>
 
 <template>
-  <div class="flex flex-1 flex-col">
+  <div class="flex flex-col h-full">
+
     <Navigation />
-    <main class="flex flex-1 flex-col">
+
+    <!-- Zone centrale flexible -->
+    <main class="flex flex-col flex-1 min-h-0">
       <RouterView />
     </main>
+
     <MessageFooter/>
+
   </div>
 </template>
+
--- src/views/PalettesView.vue
+++ src/views/PalettesView.vue
@@ -1,1 +1,47 @@
-<template></template>
+<script setup lang="ts">
+    import { palettes } from "../data/paletteExamples.ts"
+    import type { Palette } from "../types/palette.ts"
+    import CartePalette from "../components/CartePalette.vue"
+    import { ref } from "vue"
+    const paletteChoisie = ref<Palette | null>(null)
+</script>
+
+
+
+<template>
+
+<div class="container-palettes flex w-full h-full px-2">
+
+    <div class="palettes w-1/4 h-full overflow-y-auto grid gap-2 scrollbar-hide ">
+
+        <CartePalette 
+            v-for="palette in palettes"
+            :key="palette.id"
+            :palette="palette"
+            @click="paletteChoisie = palette"
+            :selected="paletteChoisie?.id === palette.id"
+            :class="{
+                'bg-slate-200 text-black rounded-l-xl': paletteChoisie?.id === palette.id
+            }"
+        />
+    </div>
+
+    <div class="couleurs w-3/4 h-full overflow-y-auto bg-slate-200 rounded-r-xl">
+
+    </div>
+
+</div>
+
+</template>
+
+<style>
+/* Hide scrollbar but keep scrolling */
+.scrollbar-hide::-webkit-scrollbar {
+  display: none;
+}
+
+.scrollbar-hide {
+  -ms-overflow-style: none; /* IE & Edge */
+  scrollbar-width: none; /* Firefox */
+}
+</style>
