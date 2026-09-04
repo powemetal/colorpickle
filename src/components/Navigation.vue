a43eecfee222fb40f2f0579dcdfc707141e770e8
Mathieu Gosselin <math.gosselin@gmail.com>
2026-09-03 20:13:54 -0400

    fix: les boutons de la navbar ont une apparence plus cohérente

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -12,42 +12,44 @@ const messageSauvegarde = "Données sauvegardées!";
   <nav>
     <div class="flex transition">
       <div
-        class="bg-black/20 rounded-full w-24 h-24 min-w-24 flex flex-1 justify-center items-center m-4 hover:border-4 hover:border-solid hover:border-black/30"
+        class="bg-black/20 rounded-full w-24 h-24 min-w-24 flex flex-1 justify-center items-center m-4 hover:border-4 hover:border-solid hover:border-black/10"
       >
-        <RouterLink :to="{ name: 'accueil' }"
-          ><img class="w-16 mix-blend-overlay" :src="logo" alt="Logo"
-        /></RouterLink>
+        <RouterLink :to="{ name: 'accueil' }">
+          <img class=" w-16 mix-blend-overlay" :src="logo" alt="Logo"/>
+        </RouterLink>
       </div>
+
 
       <div class="flex items-start justify-end m-4 w-full">
         <div class="flex">
           <div
-            class="flex bg-black/20 rounded-3xl w-18 h-12 justify-center items-center mix-blend-overlay mx-2 hover:border-4 hover:border-solid hover:border-black/30"
+            class="flex bg-black/20 rounded-3xl w-18 h-12 justify-center items-center mx-2 hover:border-4 hover:border-solid hover:border-black/10"
           >
             <button @click="recupererCouleurPick">
-              <i class="fa-solid fa-eye-dropper fa-2xl mix-blend-overlay"></i>
+              <i class=" fa-solid fa-eye-dropper fa-2xl mix-blend-overlay"></i>
             </button>
           </div>
           <div
-            class="flex bg-black/20 rounded-3xl w-18 h-12 justify-center items-center mix-blend-overlay mx-2 hover:border-4 hover:border-solid hover:border-black/30"
+            class="flex bg-black/20 rounded-3xl w-18 h-12 justify-center items-center  mx-2 hover:border-4 hover:border-solid hover:border-black/10"
           >
             <RouterLink :to="{ name: 'palettes' }"
-              ><i class="fa-solid fa-swatchbook fa-2xl mix-blend-overlay"></i
-            ></RouterLink>
+              >
+              <i class=" fa-solid fa-swatchbook fa-2xl mix-blend-overlay "></i>
+            </RouterLink>
           </div>
           <div
-            class="flex bg-black/20 rounded-3xl w-18 h-12 justify-center items-center mx-2 mix-blend-overlay hover:border-4 hover:border-solid hover:border-black/30"
+            class="flex bg-black/20 rounded-3xl w-18 h-12 justify-center items-center mx-2  hover:border-4 hover:border-solid hover:border-black/10"
           >
             <button @click="useMessage().afficherMessage(messageSauvegarde)">
-              <i class="fa-solid fa-floppy-disk fa-2xl"></i>
+              <i class=" fa-solid fa-floppy-disk mix-blend-overlay fa-2xl "></i>
             </button>
           </div>
           <div
-            class="flex bg-black/20 rounded-3xl w-18 h-12 justify-center items-center mx-2 mix-blend-overlay hover:border-4 hover:border-solid hover:border-black/30"
+            class="flex bg-black/20 rounded-3xl w-18 h-12 justify-center items-center mx-2  hover:border-4 hover:border-solid hover:border-black/10"
           >
-            <RouterLink :to="{ name: 'utilitaires' }"
-              ><i class="fa-solid fa-palette fa-2xl"></i
-            ></RouterLink>
+            <RouterLink :to="{ name: 'utilitaires' }">
+                <i class="fa-solid fa-palette mix-blend-overlay fa-2xl "></i>
+            </RouterLink>
           </div>
         </div>
       </div>
