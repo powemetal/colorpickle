f08530d3d111da52ced706d49a20fb2a89ad041f
powemetal <francisbvrt@gmail.com>
2026-09-04 22:32:19 -0400

    [Feat] Implementation de suppression de de palette et de suppression de couleur

--- src/views/PalettesView.vue
+++ src/views/PalettesView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
     import { usePalette } from "../composables/usePalette.ts"
-    const { palettes, ajouterPalette } = usePalette();
+    const { palettes, ajouterPalette, supprimerPalette, supprimerCouleur } = usePalette();
     import type { Palette } from "../types/palette.ts"
     import type { Couleur } from "../types/couleur.ts"
     import CartePalette from "../components/CartePalette.vue"
@@ -9,9 +9,15 @@
     import AjoutPalette from "../components/AjoutPalette.vue"
     import ChoisirPalette from "../components/ChoisirPalette.vue"
     import ChoixCouleurs from "../components/ChoixCouleurs.vue"
-    import { ref } from "vue"
-    const paletteChoisie = ref<Palette | null>(null)
+    import { ref, computed } from "vue"
+    // const paletteChoisie = ref<Palette | null>(null)
     const couleurChoisie = ref<Couleur | null>(null)
+    const paletteChoisieId = ref<number | null>(null)
+    const paletteChoisie = computed(() =>
+        palettes.value.find(p => p.id === paletteChoisieId.value) || null
+    )
+
+
     
     const paletteVide : Palette = {
         id: 0,
@@ -28,46 +34,117 @@
 
 <template>
 
-<div class="container-palettes flex w-full h-full px-2">
+<div class="flex flex-col h-full">
 
-    <div class="palettes w-1/4 h-full overflow-y-auto grid gap-2 scrollbar-hide ">
+    
+    <div class="flex-1 min-h-0">
+        <div class="container-palettes flex w-full h-full px-2">
 
-        <CartePalette 
-            v-for="palette in palettes"
-            :key="palette.id"
-            :palette="palette"
-            @click="paletteChoisie?.id === palette.id ? paletteChoisie = null : paletteChoisie = palette"
-            :selected="paletteChoisie?.id === palette.id"
-            :class="{ 'bg-[#e2e8f04D] text-black rounded-l-xl': paletteChoisie?.id === palette.id }"
-        />
-        <CartePalette
-            :key="paletteVide.id"
-            :palette="paletteVide"
-            :selected="paletteChoisie?.id === paletteVide.id"
-            @click="paletteChoisie?.id === paletteVide.id ? paletteChoisie = null : paletteChoisie = paletteVide"
-            :class="{ 'bg-[#e2e8f04D] text-black rounded-l-xl': paletteChoisie?.id === paletteVide.id }"
-        />
+    
+            <div class="palettes w-1/4 h-full overflow-y-auto grid gap-2 scrollbar-hide">
+                <CartePalette 
+                    v-for="palette in palettes"
+                    :key="palette.id"
+                    :palette="palette"
+                    @click="paletteChoisieId === palette.id ? paletteChoisieId = null : paletteChoisieId = palette.id"
+                    :selected="paletteChoisie?.id === palette.id"
+                    :class="{ 'bg-[#e2e8f04D] text-black rounded-l-xl': paletteChoisie?.id === palette.id }"
+                />
 
+                <CartePalette
+                    :key="paletteVide.id"
+                    :palette="paletteVide"
+                    :selected="paletteChoisieId === paletteVide.id"
+                    @click="paletteChoisieId === paletteVide.id ? paletteChoisieId = null : paletteChoisieId = paletteVide.id"
+                    :class="{ 'bg-[#e2e8f04D] text-black rounded-l-xl': paletteChoisie?.id === paletteVide.id }"
+                />
+            </div>
+
+    
+            <div class="couleurs w-3/4 h-full overflow-y-auto rounded-r-xl scrollbar-hide"
+                :class="{ 'bg-[#e2e8f04D]': paletteChoisie != null }">
+
+                <div v-if="paletteChoisieId === null" class="flex flex-col h-full">
+                    <ChoisirPalette/>
+                </div>
+
+                <div v-if="paletteChoisieId === 0" class="flex flex-col h-full">
+                    <AjoutPalette :ajouterPalette="ajouterPalette" />
+                </div>
+
+                <ChoixCouleurs 
+                    v-if="paletteChoisie && paletteChoisie.id !== 0"
+                    :palette="paletteChoisie"
+                    :paletteChoisieId="paletteChoisieId" 
+                    @select="couleurChoisie = $event"
+                />
+            </div>
+
+        </div>
     </div>
 
-    <div class="couleurs w-3/4 h-full overflow-y-auto rounded-r-xl scrollbar-hide"
-    :class="{
-         'bg-[#e2e8f04D]': paletteChoisie != null
-     }"
-    >
-        <div v-if="!paletteChoisie" class="flex flex-col h-full"><ChoisirPalette/></div>
+<div class="flex w-full h-[10%] text-white">
 
-        <div v-if="paletteChoisie?.id === 0" class="flex flex-col h-full"><AjoutPalette :ajouterPalette="ajouterPalette" /></div>
+    <div class="mx-5 flex items-center">
+        <button
+            class="px-2 py-1 
+            bg-white/10 
+            text-white 
+            rounded 
+            border border-white/10 
+            hover:bg-white/20 
+            transition-all duration-150 
+            whitespace-nowrap"
+            @click="paletteChoisie && supprimerPalette(paletteChoisie); paletteChoisieId=null"
+        >
+            Supprimer
+        </button>
+    </div>
 
-        <ChoixCouleurs 
-          v-if="paletteChoisie && paletteChoisie.id !==0"
-          :palette="paletteChoisie" 
-        />
+    <div class="flex items-center">
+        <input type="text" class="border w-40 mr-1">
+    </div>
+
+        <div class="flex items-center  mx-auto">
+        <button
+        class="px-2 py-1
+                bg-white/10
+                text-white
+                rounded
+                border border-white/10
+                hover:bg-white/20
+                transition-all duration-150
+                whitespace-nowrap"
+
+        >
+        Enregistrer
+        </button>
+        </div>
+
+    <div class="flex items-center ml-auto mr-3">
+        <button
+        class="px-2 py-1
+                bg-white/10
+                text-white
+                rounded
+                border border-white/10
+                hover:bg-white/20
+                transition-all duration-150
+                whitespace-nowrap"
+                @click="couleurChoisie && paletteChoisie && supprimerCouleur(couleurChoisie.id, paletteChoisie); couleurChoisie = null"
 
 
+
+        >
+        Supprimer
+        </button>
     </div>
 
 </div>
+
+
+</div>
+
 
 </template>
 
